555fcb977ad39b534bd781a1237a85f44cafbb1d
Kody Robinson <66851553+krobinson96@users.noreply.github.com>
2026-03-10 15:49:17 -0700

    Object construction and contains, addSymbol and main functions changed to add stability. Added ability to remove a term
    A host of changes, including a change to the constructor to omit empty definitions, the contains method now returns a string of the query status, the addSymbol method now has redundancy handling, the main function has been changed to fix a bug and allow proper sandbox testing when imported, a break to program during grammar file input and generating a symbol was added and the ability to remove a term along with a main menu update reflecting this

--- GrammarBuddyHelper.py
+++ GrammarBuddyHelper.py
@@ -6,8 +6,6 @@ the rules of the grammar are compiled within a list with each definition constit
 
 This class accepts text files (.txt) and the default save location is within BNF Forms/grammar.txt.
 
-...
-
 Attributes
 ----------
 rules : str[]
@@ -31,6 +29,8 @@ addExpression(symbol, expression)
     this function determines whether or not an expression represents a symbol and adds the expression if not
 saveMap(filename='BNF Forms/grammar.txt')
     this function saves the current grammar as a text file
+updateMap(filename='BNF Forms/grammar.txt')
+    this function updates the current grammar from a text file
 Written by Kody L. Robinson 2025
 """
 import random, sys
@@ -41,7 +41,6 @@ class GrammarBuddyHelper:
         """
         This is the default constructor, custom delimiters can be used with '::=' and '|' 
         representing the default delimiters for the symbols and expressions respectively
-        ...
 
         Parameters
         ----------
@@ -70,13 +69,15 @@ class GrammarBuddyHelper:
             line = i.split(symDelim)
             expressions = line[1].split(exprDelim)
             rule = {line[0]: expressions}
+            if not exprDelim in rule and not line[0]:
+                print("Empty definition ...")
+                continue
             self.langMap.update(rule)
    
     def generate(self, symbol):
         """
         This method generates an expression based upon a given symbol. 
         Returns "Symbol not found in grammar" if the given symbol is not found within the grammar
-        ...
 
         Parameters
         ----------
@@ -106,7 +107,6 @@ class GrammarBuddyHelper:
         """
         This method checks whether or not a term is a symbol. If the term is not a symbol, all of the 
         expressions of each symbol are checked
-        ...
 
         Parameters
         ----------
@@ -118,31 +118,36 @@ class GrammarBuddyHelper:
         bool
             an unnamed boolean value representing whether or not the term was found within the grammar
         """
-        if term in self.langMap.keys():
-            return True
-        for i in self.langMap.values():
-            if term in i:
-                return True
-        return False
+        if term in self.langMap.keys() and term:
+            return f"{term} is a symbol."
+        else:
+            for i in self.langMap.keys():
+                for j in self.langMap[i]:
+                    if term in j:
+                        return f"{term} is an expression."
+        return "{term} is not present within the grammar."
 
     def addSymbol(self, symbol):
         """
         This method checks whether or not a given symbol is within the grammar. If not, the symbol is added
-        ...
 
         Parameters
         ----------
         symbol : str
             this is the symbol to be added
         """
-        add = {symbol: []}
-        self.langMap.update(add)
-
+        if symbol in self.langMap.keys():    
+            choice = pyip.inputYesNo("Symbol {symbol} is already in the grammar. Would you like to overwrite it?\n", blank=True)
+            if choice != 'no':
+                add = {symbol: []}
+                self.langMap.update(add)
+        else:
+            add = {symbol: []}
+            self.langMap.update(add)
     def addExpression(self, symbol, expression):
         """
         This method checks whether or not a given expression is found for the given symbol. If not,
         the symbol is defined as the expression and is saved into the grammar
-        ...
 
         Parameters
         ----------
@@ -162,7 +167,6 @@ class GrammarBuddyHelper:
     def saveMap(self, filename='BNF Forms/grammar.txt'):
         """
         This method allows the grammar to be saved in a text file with each line constituting a symbol defintion
-        ...
 
         Parameters
         ----------
@@ -183,11 +187,18 @@ class GrammarBuddyHelper:
         file.close()         
 
     def updateMap(self, filename):
+        """
+        This method allows the current grammar to be updated with a given filename
+        
+        Parameters
+        ----------
+        filename : str 
+            the filename that the grammar will be saved as 
+        """
         if '.txt' not in filename:
             filename += '.txt'    
         with open(filename, 'r', encoding='utf-8') as file:
             for rule in file:
-                #self.addSymbol(rule.strip().split(self.symDelim)[0])
                 for expr in rule.strip().split(self.symDelim)[1].split(self.exprDelim):
                     self.addExpression(rule.strip().split(self.symDelim)[0].strip(), expr.strip())
 
@@ -202,9 +213,9 @@ def main():
         print(f"Uh oh\n{e}")
     gb = GrammarBuddyHelper(rules) # Constructing GrammarBuddyHelper object
 
-    gb.addExpression('<x>','<s>') # Adding sample expression
+    gb.addExpression('<x>','<expression>') # Adding sample expression
     for i in range(100):
-        print(f'{i+1}: {gb.generate('<expression>')}') # Generate 100 sample expressions
+        print(f'{i+1}: {gb.generate('<x>')}') # Generate 100 sample expressions
     gb.saveMap("BNF Forms/testgrammar.txt") # Saving updated grammar
 
 if __name__ == "__main__":
@@ -214,6 +225,7 @@ if __name__ == "__main__":
         while not rules:
             filename = input("Path of grammar file? Press ENTER to skip\n")
             if not filename:
+                gb = GrammarBuddyHelper([])
                 break
             try:
                 with open(filename, 'r', encoding='utf-8') as file:
@@ -228,21 +240,23 @@ if __name__ == "__main__":
         gb = GrammarBuddyHelper([])
     flag = True
     while flag:
-        choice = pyip.inputMenu(['Generate a symbol', 'Generate a number of symbols', 'List symbols', 'List expressions', 'Add a symbol', 'Add an expression', 'Does it contain?', 'Open a grammar text file', 'Save grammar to a text file', 'Exit'], numbered=True)
+        choice = pyip.inputMenu(['Generate a symbol', 'Generate a number of symbols', 'List symbols', 'List expressions', 
+                                 'Add a symbol', 'Add an expression', 'Remove a term', 'Does it contain?', 
+                                 'Open a grammar text file', 'Save grammar to a text file', 'Exit'], 
+                                 numbered=True)
 
         match choice:
             case 'Generate a symbol':
                 if 0 == len(gb.langMap.keys()): 
                     print("Cannot generate a big nothing symbol!")
                     continue
-                print("Press ENTER to skip")
-                choice = pyip.inputMenu(list(gb.langMap.keys()), numbered=True, blank=True)
+                choice = pyip.inputMenu(list(gb.langMap.keys()), prompt="Which symbol would you like to generate? Press ENTER to exit\n", numbered=True, blank=True)
                 print(gb.generate(choice))
             case 'Generate a number of symbols':
                 if 0 == len(gb.langMap.keys()):
                     print("Cannot generate multiple big nothing symbols!")
                     continue
-                symbol = pyip.inputMenu(list(gb.langMap.keys()), prompt="Which symbol would you like to generate? Press ENTER to exit\n", blank=True, numbered=True)
+                symbol = pyip.inputMenu(list(gb.langMap.keys()), prompt="Which symbol would you like to generate? Press ENTER to exit\n", numbered=True, blank=True)
                 if symbol: 
                     print(f"How many {symbol}'s would you like to print?")
                     symbols = pyip.inputNum(greaterThan=-1)
@@ -255,19 +269,47 @@ if __name__ == "__main__":
                 if 0 == len(gb.langMap.keys()):
                     print("Cannot print a list of big nothing burgers!")
                     continue
-                symbol = pyip.inputMenu(list(gb.langMap.keys()), prompt="Which expressions would you like to list?\n", numbered=True, blank=True)
+                symbol = pyip.inputMenu(list(gb.langMap.keys()), prompt="Which expressions would you like to list? Press ENTER to exit\n", numbered=True, blank=True)
                 if symbol and len(gb.langMap[symbol]) > 0:
                     for i in range(len(gb.langMap[symbol])):
                         print(f'{i+1}: {gb.langMap[symbol][i]}')
                 else:
                     print("Symbol has no definitions!")        
             case 'Add a symbol':
+            #TODO: enter exit
                 symbol = input("What symbol would you like to add?\n")
                 gb.addSymbol(symbol)
             case 'Add an expression':
+            #TODO: enter exit
                 symbol = input("What symbol does this expression define?\n")
                 expression = input("What is the expression?\n")
                 gb.addExpression(symbol=symbol, expression=expression)
+            case 'Remove a term':
+                choice = pyip.inputMenu(['Symbol', 'Expression'], prompt="Would you like to remove a symbol or an expression? Press ENTER to exit\n", numbered=True, blank=True)
+                match choice:
+                    case 'Symbol':
+                        for i in gb.langMap.keys():
+                            print(i)
+                        symbol = input("What symbol would you like to remove? Press ENTER to exit\n")
+                        if symbol and symbol in gb.langMap.keys():
+                            del gb.langMap[symbol]
+                        elif symbol and symbol not in gb.langMap.keys():
+                            print("Symbol not in grammar!")
+                    case 'Expression':
+                        if len(gb.langMap.keys()) == 0:
+                            print("Cannot remove anything from a big nothing burger!")
+                            continue
+                        symbol = pyip.inputMenu(list(gb.langMap.keys()), prompt="What symbol is the expression you want to remove? Press ENTER to exit \n", numbered=True, blank=True)
+                        if not symbol:
+                            continue
+                        expression = input("What expression would you like to remove? Press ENTER to exit\n")                        
+                        if not expression:
+                            continue
+                        elif expression in gb.langMap[symbol]:
+                            gb.langMap[symbol].remove(expression)
+                            print(f'Expression {expression} removed from grammar!')
+                    case _:
+                        continue
             case 'Does it contain?':
                 term = input("What term would you like to check?\n")
                 print(gb.contains(term))
